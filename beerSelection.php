897e9b3a052cea0fb6c2af8fff3acc8adcabc265
Zak <46013015+Zymbol@users.noreply.github.com>
2021-05-14 17:26:18 -0700

    new file

---
+++ beerSelection.php
@@ -0,0 +1,8 @@
+<?php 
+include "./connect.php";
+include "./header.php";?>
+<div data-aos="flip-left"
+data-aos-easing="ease-out-cubic"
+data-aos-duration="2000">
+<?php include "./nav.php"; ?>
+</div>
